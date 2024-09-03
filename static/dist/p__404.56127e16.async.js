9cc1fd34fbcb0995b77d338ef03f1916089bf8a5
echoyl <yliang_1987@126.com>
2024-09-03 16:34:44 +0800

    dist 产物分包

---
+++ static/dist/p__404.56127e16.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[571],{9826:function(e,t,n){n.r(t);var o=n(83832),a=n(67294),s=n(85893),u=(0,a.lazy)(function(){return Promise.resolve().then(n.bind(n,36942))});t.default=function(){return(0,s.jsx)(a.Suspense,{fallback:(0,s.jsx)(o.S,{}),children:(0,s.jsx)(u,{})})}}}]);
