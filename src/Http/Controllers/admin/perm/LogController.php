b3ac81a9e72ee70f800d6e8ed07f7f7430bde897
echoyl <yliang_1987@126.com>
2024-09-19 16:19:49 +0800

    优化操作日志

--- src/Http/Controllers/admin/perm/LogController.php
+++ src/Http/Controllers/admin/perm/LogController.php
@@ -1,59 +1,60 @@
 <?php
-
 namespace Echoyl\Sa\Http\Controllers\admin\perm;
-
 use Echoyl\Sa\Http\Controllers\admin\CrudController;
 use Echoyl\Sa\Models\perm\Log;
 
+
+//customer namespace start
+
+//customer namespace end
+
+/**
+ * @property \App\Services\echoyl\AdminAppService $service
+ */
 class LogController extends CrudController
 {
-    //
-	var $model;
-	var $with_column = [];
-	var $search_config = [
-		["name" => "created_at","columns" => ["created_at"],"where_type" => "whereBetween"],
-		["name" => "keyword","columns" => ["type"],"where_type" => "like"]
-	];
-    public function __construct(Log $model)
+	//customer property start
+	
+	//customer property end
+    public function __construct()
 	{
-		$this->model = $model;
+		parent::__construct();
 		$this->with_column = [
-			'user'=>function($q){
-				$q->select(['id','username']);
-			}
+		    'user' => function($q0){$q0->select(["id","username","roleid"]);},
 		];
+		$this->search_config = [
+		    [
+		        'name' => 'created_at',
+		        'columns' => [
+		            'created_at',
+		        ],
+		        'where_type' => 'whereBetween',
+		    ],
+		    [
+		        'name' => 'keyword',
+		        'columns' => [
+		            'type',
+		        ],
+		        'where_type' => 'like',
+		    ],
+		];
+		$this->model = new Log();
+		$this->model_class = Log::class;
+		//customer construct start
+		
+		//customer construct end
 	}
 
+	//customer code start
 	public function beforePost($data, $id = 0)
 	{
 		return $this->fail([1,'操作日记不能更新']);
-	}
-
-
-	public function handleSearch($search = [])
-	{
-		$m = $this->model;
-
-		
-		
-		$startTime = request('startTime','');
-		$endTime = request('endTime','');
-
-		if($startTime)
-		{
-			$m = $m->where([['created_at','>=',$startTime]]);
-		}
-		if($endTime)
-		{
-			$m = $m->where([['created_at','<=',date("Y-m-d H:i:s",strtotime($endTime)+3600*24-1)]]);
-		}
-
-		return [$m,$search];
 	}
 
 	public function beforeDestroy($m)
 	{
 		return $m->where([['id','=',0]]);
 	}
-    
+	//customer code end
+	
 }
