b388f6d6c9ce29d6117215e1d190b79ec0c3358e
echoyl <yliang_1987@126.com>
2024-10-23 16:47:05 +0800

    fix:dist

---
+++ static/dist/p__404.23d352cb.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[571],{9826:function(l,t,n){n.r(t);var o=n(60297),a=n(67294),s=n(85893),u=(0,a.lazy)(function(){return Promise.resolve().then(n.bind(n,36942))});t.default=function(){return(0,s.jsx)(a.Suspense,{fallback:(0,s.jsx)(o.Z,{}),children:(0,s.jsx)(u,{})})}}}]);
