70613db368c9c3d520991367c15ce3068c8968c5
echoyl <yliang_1987@126.com>
2025-01-09 16:25:10 +0800

    dist v0.9

---
+++ static/dist/p__dev__test.5c4ff79e.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[263],{76235:function(e,t,n){n.r(t),n.d(t,{default:function(){return d}});var o=n(15460),u=n(85893),s=function(){return(0,u.jsx)(o.Z,{pageMenu:{model_id:10019}})};function d(){return(0,u.jsx)(s,{})}}}]);
