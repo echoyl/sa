8659e2e3cc5a729e7c5bef25efb9c7e8f41a6f25
echoyl <inandan@126.com>
2026-08-04 13:47:12 +0800

    fix:app.php

--- static/deadmin/app.php
+++ static/deadmin/app.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Middleware\WebFirst;
+use App\Http\Middleware\Wx;
+use App\Http\Middleware\WxApi;
+use App\Http\Middleware\WxappApi;
+use App\Http\Middleware\WxappApiUser;
 use Echoyl\Sa\Exceptions\AException;
+use Echoyl\Sa\Helpers\ResponseEnum;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -14,19 +20,26 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectTo(fn () => null);
-        $middleware->alias([]);
+        $middleware->alias([
+            'webFirst' => WebFirst::class,
+            'wx' => Wx::class,
+            'wxApi' => WxApi::class,
+            'wxappApi' => WxappApi::class,
+            'wxappApiUser' => WxappApiUser::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->reportable(function (AException $e) {
             //
         });
         $exceptions->render(function (Throwable $e) {
+            [$code] = ResponseEnum::CLIENT_HTTP_UNAUTHORIZED_EXPIRED;
             if (request()->ajax() || request()->wantsJson() || request()->isJson()) {
                 return response()->json([
-                    'msg' => $e->getMessage(),
+                    'msg' => implode("\r", [$e->getMessage(), $e->getFile().$e->getLine()]),
                     'code' => match (true) {
                         $e instanceof AException => $e->getCode(),
-                        $e instanceof AuthenticationException => 1001,
+                        $e instanceof AuthenticationException => $code,
                         default => 1,
                     },
                     'data' => [],
